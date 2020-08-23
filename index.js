4d58780603db638c3b120e7485e4e13cdaf68ac9
Liliia Smelova <liliia.smelova@outlook.com>
2020-08-23 22:25:17 +0300

    3.3-3.4: Phonebook backend

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const express = require("express");
 const app = express();
 
+let persons = [
+    {
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1,
+    },
+    {
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 2,
+    },
+    {
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 3,
+    },
+    {
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 4,
+    },
+    {
+        name: "w",
+        number: "w",
+        id: 5,
+    },
+];
+
 app.get("/api/persons", (request, response) => {
-    
     if (persons) {
         response.json(persons);
     } else {
@@ -11,45 +38,34 @@ app.get("/api/persons", (request, response) => {
 });
 
 app.get("/info", (request, response) => {
-
     let entries = persons.length;
     var today = new Date();
     var date = today.toUTCString();
 
-        response.send(`<p>Phonebook has info for ${entries} people </p> <p>${date}</p>` );
-    
+    response.send(
+        `<p>Phonebook has info for ${entries} people </p> <p>${date}</p>`
+    );
+});
+
+app.get("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id);
+    const person = persons.find((person) => person.id === id);
+    console.log(person);
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    }
 });
 
 
+app.delete("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id);
+    persons = persons.filter((person) => person.id !== id);
 
-let persons = [
-    {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": 1
-    },
-    {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": 2
-    },
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 3
-    },
-    {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": 4
-    },
-    {
-      "name": "w",
-      "number": "w",
-      "id": 5
-    }
-  ]
+    response.status(204).end();
+});
 
-
-  const PORT = 3001;
-  app.listen(PORT);
+const PORT = 3001;
+app.listen(PORT);
