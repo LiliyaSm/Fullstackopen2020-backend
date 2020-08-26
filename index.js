8dad4fd38398dad7ee0c7dfba0577d6dec296b5d
Liliia Smelova <liliia.smelova@outlook.com>
2020-08-26 15:42:04 +0300

    3.16: Phonebook database, step4, add error handlers

--- index.js
+++ index.js
@@ -109,10 +109,35 @@ app.post("/api/persons", (request, response) => {
     console.log(newPerson);
 
     // data sent back in the response is formatted
-    newPerson.save().then((savedPerson) => {
-        response.json(savedPerson);
-    });
+    newPerson
+        .save()
+        .then((savedPerson) => {
+            response.json(savedPerson);
+        })
+        .catch((error) => next(error));
 });
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: "unknown endpoint" });
+};
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    // id doesn't match the mongo identifier format
+    if (error.name === "CastError") {
+        return response.status(400).send({ error: "malformatted id" });
+    }
+
+    //  pass the error forward to the default Express error handler.
+    next(error);
+};
+
+// handler of requests with result to errors
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
